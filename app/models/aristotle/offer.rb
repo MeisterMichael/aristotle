05230e24fff67b21c2004006a2280b36409e0101
Michael Ferguson <meister@spacekace.com>
2020-01-29 13:55:02 -0800

    tagging offers

--- app/models/aristotle/offer.rb
+++ app/models/aristotle/offer.rb
@@ -1,8 +1,20 @@
+require 'acts-as-taggable-array-on'
+
 module Aristotle
 	class Offer < ApplicationRecord
 
 		belongs_to :product, required: false
 		enum offer_type: { 'subscription' => 1, 'default' => 0, 'renewal' => 2 }
+
+		acts_as_taggable_array_on :tags
+
+		def tags_csv
+			tags.join(',')
+		end
+
+		def tags_csv=(str)
+			self.tags = str.split(',')
+		end
 
 	end
 end
