38a6d999b740b58c8492cedefd3dfb7407c70594
Michael Ferguson <meister@spacekace.com>
2022-11-08 10:55:56 -0800

    updated migration

--- lib/tasks/install_files/20221010152500_order_event_ids_adj_migration.rb
+++ lib/tasks/install_files/20221010152500_order_event_ids_adj_migration.rb
@@ -4,25 +4,22 @@ class OrderEventIdsAdjMigration < ActiveRecord::Migration[5.1]
 		rename_column :aristotle_transaction_items, :event_client_id, :src_event_client_id
 		rename_column :aristotle_transaction_items, :event_id, :src_event_id
 		change_table :aristotle_transaction_items do |t|
-			t.belongs_to :event, default: nil, index: { name: 'index_aristotle_transaction_items_on_event' }
+			t.integer :event_client_purchase_index, default: 1
+			t.index [:event_client_purchase_index, :src_event_client_id], name: 'index_aristotle_transaction_items_on_purchase_index'
 		end
 
 		rename_column :aristotle_transaction_skus, :event_client_id, :src_event_client_id
 		rename_column :aristotle_transaction_skus, :event_id, :src_event_id
 		change_table :aristotle_transaction_skus do |t|
-			t.belongs_to :event, default: nil, index: { name: 'index_aristotle_transaction_skus_on_event' }
+			t.integer :event_client_purchase_index, default: 1
+			t.index [:event_client_purchase_index, :src_event_client_id], name: 'index_aristotle_transaction_skus_on_purchase_index'
 		end
 
 		rename_column :aristotle_orders, :event_client_id, :src_event_client_id
 		rename_column :aristotle_orders, :event_id, :src_event_id
 		change_table :aristotle_orders do |t|
-			t.belongs_to :event, default: nil, index: { name: 'index_aristotle_orders_on_event' }
-		end
-
-		rename_column :aristotle_subscriptions, :event_client_id, :src_event_client_id
-		rename_column :aristotle_subscriptions, :event_id, :src_event_id
-		change_table :aristotle_subscriptions do |t|
-			t.belongs_to :event, default: nil, index: { name: 'index_aristotle_subscriptions_on_event' }
+			t.integer :event_client_purchase_index, default: 1
+			t.index [:event_client_purchase_index, :src_event_client_id], name: 'index_aristotle_orders_on_purchase_index'
 		end
 
 	end
